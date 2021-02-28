798a54bb0efffa9dde6f07009e01d83955731e7c
Ankit Sachan <a.sachan545@gmail.com>
2021-02-28 19:55:20 +0530

    changed master message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World Monkey.</h1>
-      <h1>This is a part of Develop branch</h1>
+      <h1>I am a master.</h1>
     </div>
   );
 }
